b1d696df10767b800951b79d690922f1dfc9809c
JethroNatividad <natividad.jet@gmail.com>
2025-09-13 19:49:13 +0800

    feat: implement multiple cvs

--- main.py
+++ main.py
@@ -2,29 +2,40 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
 import json
 
+options = Options()
 
-driver = webdriver.Firefox()
+options.set_preference(
+    "browser.helperApps.neverAsk.saveToDisk", "application/pdf,application/octet-stream"
+)
+options.set_preference("pdfjs.disabled", True)  # disable built-in pdf viewer
+driver = webdriver.Firefox(options=options)
+
 driver.get("https://resumave.vercel.app/edit?tab=personalInfo")
 
 driver.implicitly_wait(2)
 
-with open("job.json", "r") as f:
-    new = json.load(f)
+with open("jobs.json", "r") as f:
+    jobs = json.load(f)
+    for job in jobs:
+        job_str = json.dumps(job)
 
-    job = json.dumps(new)
-
-    driver.execute_script(
-        f"localStorage.setItem('resume-storage', JSON.stringify({job}))"
-    )
-    driver.refresh()
-    # Wait for a tag that has download="Arjun_Mehta_Resume.pdf" (new.name replace space with _ and add _Resume.pdf at the end)
-    resume_name = new["state"]["personalInfo"]["name"].replace(" ", "_") + "_Resume.pdf"
-    WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located(
-            (By.CSS_SELECTOR, f'a[download="{resume_name}"]')
+        driver.execute_script(
+            f"localStorage.setItem('resume-storage', JSON.stringify({job_str}))"
         )
-    )
-    # Click on the tag
-    driver.find_element(By.CSS_SELECTOR, f'a[download="{resume_name}"]').click()
+
+        driver.refresh()
+        resume_name = (
+            job["state"]["personalInfo"]["name"].replace(" ", "_") + "_Resume.pdf"
+        )
+
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, f'a[download="{resume_name}"]')
+            )
+        )
+
+        driver.find_element(By.CSS_SELECTOR, f'a[download="{resume_name}"]').click()
+driver.quit()
